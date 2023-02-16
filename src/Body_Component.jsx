f934a0832dffe6fce032ffa8e6922bafaac96745
victor <viktall007@gmail.com>
2023-02-16 21:59:11 +0100

    PII

--- src/Body_Component.jsx
+++ src/Body_Component.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Box} from '@mui/material'
+import Styles from './Asset/mystyles';
 
 
 const MainComp=({QSTNS}) =>{
@@ -9,28 +10,7 @@ const MainComp=({QSTNS}) =>{
   const [correct, setCorrect]= useState(0)
   const [notCorrect, setNotCorrect]= useState(0)
   
-
-  /*const btn={
-
-    display:'flex',
-    width: 500,
-    height: 70,
-    justifyContent:'center',
-    alignItems:'center',
-    mx: 'auto',
-    my:1,
-    backgroundColor: 'primary.dark',
-    '&:hover': {
-      backgroundColor: 'primary.main',
-      opacity: [0.9, 0.8, 0.7],
-    },
-    
-}     
-*/
-
-
   
-
   const HandleEvent =({id, isCorrect})=> {
 
 
@@ -54,34 +34,23 @@ const MainComp=({QSTNS}) =>{
             setNotCorrect(0)
             setScore(prev=>prev+1)
           }
-      
         
   }
       
       
-      
-
-  console.log(score, count)
+console.log(score, count)
 return(
 
-      <Box sx={{ width:950, mx:'auto', pb:1}}>
-        <Box sx={{display:'flex', justifyContent:'center', height:60, alignItems:'center', fontSize:'25px', bgcolor:'#178CA4'}}> Question {[count+1]}</Box>
+      <Box sx={Styles.outermostbox}>
+        <Box sx={Styles.outerbox}> Question {[count+1]}</Box>
 
-        <Box sx={{display:'flex', height:110, justifyContent:'center', alignItems:'center', fontSize:'40px', bgcolor:'#B3DEE5'}}>{QSTNS[count].questions}</Box>
+        <Box sx={Styles.innerbox}>{QSTNS[count].questions}</Box>
         
             {QSTNS[count].options.map((t, i)=>(
               <Box 
-                  onClick={()=>HandleEvent(t)} key={i} 
-                    sx={{display:'flex',
-                          width: 500,
-                          height: 70,
-                          justifyContent:'center',
-                          alignItems:'center',
-                          mx: 'auto',
-                          my:1, 
-                          cursor:'pointer',
-                          backgroundColor:  correct===t.id? '#00ff00':notCorrect===t.id? '#ff0000':'#cccccc',
-                          }}>
+                  onClick={()=>HandleEvent(t)} 
+                  key={i} 
+                  sx={{ ...Styles.btn, backgroundColor: correct===t.id? '#00ff00':notCorrect===t.id? '#ff0000':'#cccccc'}}>
 
                             {t.optn}
               </Box>
